77b9eff4ac66d98ff705fd9f5c3872d7c03b11e3
Leonardo Uieda <leouieda@gmail.com>
2016-05-08 13:57:49 -0300

    Add gparted to install script

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,8 +4,10 @@ DEV="git vim meld"
 VIS="gimp inkcape shutter"
 TXT="texlive-full libreoffice-impress"
 MISC="gnome-do xclip"
+ADM="gparted"
 
 sudo apt-get install -y $DEV
 sudo apt-get install -y $MISC
 sudo apt-get install -y $VIS
 sudo apt-get install -y $TXT
+sudo apt-get install -y $ADM
