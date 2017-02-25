b5ab62cbefaff4b0bbf29c4f986c012263afea2e
Leonardo Uieda <leouieda@gmail.com>
2017-02-24 17:09:19 -1000

    Configuration for the hyper terminal
    https://hyper.is/

---
+++ .hyper.js
@@ -0,0 +1,126 @@
+module.exports = {
+  config: {
+    // default font size in pixels for all tabs
+    fontSize: 13,
+
+    // font family with optional fallbacks
+    fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+
+    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
+    cursorColor: '#fff',
+
+    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
+    cursorShape: 'BLOCK',
+
+    // color of the text
+    foregroundColor: '#fff',
+
+    // terminal background color
+    backgroundColor: '#111',
+
+    // border color (window, tabs)
+    borderColor: '#333',
+
+    // custom css to embed in the main window
+    css: '',
+
+    // custom css to embed in the terminal window
+    termCSS: '',
+
+    // set to `true` (without backticks) if you're using a Linux setup that doesn't show native menus
+    // default: `false` on Linux, `true` on Windows (ignored on macOS)
+    showHamburgerMenu: 'false',
+
+    // set to `false` if you want to hide the minimize, maximize and close buttons
+    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
+    // default: `true` on windows and Linux (ignored on macOS)
+    showWindowControls: 'true',
+
+    // custom padding (css format, i.e.: `top right bottom left`)
+    padding: '5px 5px',
+
+    // the full list. if you're going to provide the full color palette,
+    // including the 6 x 6 color cubes and the grayscale map, just provide
+    // an array here instead of a color map object
+    colors: {
+      black: '#000000',
+      red: '#ff0000',
+      green: '#33ff00',
+      yellow: '#ffff00',
+      blue: '#0066ff',
+      magenta: '#cc00ff',
+      cyan: '#00ffff',
+      white: '#d0d0d0',
+      lightBlack: '#808080',
+      lightRed: '#ff0000',
+      lightGreen: '#33ff00',
+      lightYellow: '#ffff00',
+      lightBlue: '#0066ff',
+      lightMagenta: '#cc00ff',
+      lightCyan: '#00ffff',
+      lightWhite: '#ffffff'
+
+
+    },
+
+    hyperTabs: {
+      trafficButtons: true,
+      border: false,
+      tabIconsColored: true,
+      activityColor: 'salmon',
+    },
+
+    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
+    // if left empty, your system's login shell will be used by default
+    // make sure to use a full path if the binary name doesn't work
+    // (e.g `C:\\Windows\\System32\\bash.exe` instad of just `bash.exe`)
+    // if you're using powershell, make sure to remove the `--login` below
+    shell: '',
+
+    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
+    // by default ['--login'] will be used
+    shellArgs: ['--login'],
+
+    // for environment variables
+    env: {},
+
+    // set to false for no bell
+    bell: 'false',
+
+    // if true, selected text will automatically be copied to the clipboard
+    copyOnSelect: true
+
+    // if true, on right click selected text will be copied or pasted if no
+    // selection is present (true by default on Windows)
+    // quickEdit: true
+
+    // URL to custom bell
+    // bellSoundURL: 'http://example.com/bell.mp3',
+
+    // for advanced config flags please refer to https://hyper.is/#cfg
+  },
+
+  // a list of plugins to fetch and install from npm
+  // format: [@org/]project[#version]
+  // examples:
+  //   `hyperpower`
+  //   `@company/project`
+  //   `project#1.0.1`
+  plugins: [
+      'hyperlinks',
+      'hypercwd',
+      'hyperfull',
+      'hyperterm-cursor',
+      'hyperminimal',
+      'hyper-command-gifs',
+      'hyper-material-theme',
+      'hyperterm-paste',
+      'hyper-tabs-enhanced',
+      //'hyper-tab-icons',
+  ],
+
+  // in development, you can create a directory under
+  // `~/.hyper_plugins/local/` and include it here
+  // to load it and avoid it being `npm install`ed
+  localPlugins: []
+};
