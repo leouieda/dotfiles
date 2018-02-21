02b7bdbddfb73958901678d21f1e9e81447cb3ba
Leonardo Uieda <leouieda@gmail.com>
2018-02-21 12:47:17 -1000

    Add ruby binary dir to PATH

--- .bash/variables.sh
+++ .bash/variables.sh
@@ -4,6 +4,7 @@ export PATH=$HOME/bin:$PATH
 export PATH=$HOME/pkg/bin:$PATH
 export PATH=$HOME/bin/zotero:$PATH
 export PATH=$HOME/src/tesseroids/bin:$PATH
+export PATH=$HOME/.gem/ruby/2.5.0/bin:$PATH
 
 export CONDA_PREFIX=$HOME/miniconda3
 
