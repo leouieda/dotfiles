304405a7f07ff496e25186ae36c0baa01cb2a044
Leonardo Uieda <leouieda@gmail.com>
2018-07-04 11:41:10 -1000

    Function to prune git remote branches

--- .bash/functions.sh
+++ .bash/functions.sh
@@ -1,5 +1,35 @@
 # Useful bash functions
 
+## Git
+###############################################################################
+git-poda() {
+    echo "Looking for remote branches to prune..."
+    # Capture the output to check if there were any branches to prune in the first place
+    toprune=`git remote prune origin --dry-run`
+    nbranches=`echo "$toprune" | wc -l`
+    if [[ "$nbranches" == "1" ]]; then
+        echo "The tree is clean. Move along."
+        return 0
+    fi
+    echo "$toprune"
+    read -p "Prune these merged remote branches? [y/N] " response
+    clean=`echo "$response" | tr "[:upper:]" "[:lower:]"`
+    # Set the default value to No"
+    clean="${clean:-no}"
+    if [[ "$clean" == "y" ]] || [[ "$clean" == "yes" ]]; then
+        git remote prune origin;
+    elif [[ "$clean" == "n" ]] || [[ "$clean" == "no" ]]; then
+        echo "Alright, alright. I'll stop.";
+    else
+        echo "Invalid reponse: $response"
+        return 1
+    fi
+    echo "Done."
+    return 0
+}
+###############################################################################
+
+
 ## Conda
 ###############################################################################
 condaon() {
