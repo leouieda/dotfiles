1805c4cdd19589680f5846e6ec12e758680a0d21
Leonardo Uieda <leouieda@gmail.com>
2019-11-06 17:57:25 +0000

    Add Ruby 2.6.0 to path

--- .bash/path.sh
+++ .bash/path.sh
@@ -5,3 +5,4 @@ export PATH=$HOME/pkg/bin:$PATH
 export PATH=$HOME/bin/zotero:$PATH
 export PATH=$HOME/src/tesseroids/bin:$PATH
 export PATH=$HOME/.gem/ruby/2.5.0/bin:$PATH
+export PATH=$HOME/.gem/ruby/2.6.0/bin:$PATH
