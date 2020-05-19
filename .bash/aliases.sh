d6c11cd9b335e4ff73a76531a0eac7d9c89605cb
Leonardo Uieda <leouieda@gmail.com>
2020-05-19 14:02:59 +0100

    Add alias to fix ALSA

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -47,3 +47,6 @@ alias smk='snakemake'
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+# Restart ALSA if Audacity crashes it
+alias alsafix='alsactl restore'
