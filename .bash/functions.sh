956086579fd49eb72aa89759716ee7b23fcb9a28
Leonardo Uieda <leo@uieda.com>
2024-02-05 10:01:13 -0300

    Add a function to compress a PDF

--- .bash/functions.sh
+++ .bash/functions.sh
@@ -336,9 +336,26 @@ pdfcmyk() {
     # Call it as: pdfcmyk INPUT.pdf OUTPUT.pdf
     # Based on http://zeroset.mnim.org/2014/07/14/save-a-pdf-to-cmyk-with-inkscape/
     gs -dSAFER -dBATCH -dNOPAUSE -dNOCACHE -sDEVICE=pdfwrite \
-        -sColorConversionStrategy=CMYK -dProcessColorModel=/DeviceCMYK \
-        -sOutputFile=$2 $1
+       -sColorConversionStrategy=CMYK -dProcessColorModel=/DeviceCMYK \
+       -sOutputFile=$2 $1
 }
+
+pdfcompress() {
+read -r -d '' PDFCOMPRESS_HELP <<-'EOF'
+Usage: pdfcompress [INPUT] [OUTPUT]
+
+Use Ghostscript to lower the DPI of images in a PDF.
+
+EOF
+    if [[ $# == 0 ]]; then
+        echo "$PDFCOMPRESS_HELP";
+        return 0;
+    fi
+    gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook \
+       -dNOPAUSE -dQUIET -dBATCH -dDetectDuplicateImages \
+       -dCompressFonts=true -sOutputFile=$2 $1
+}
+
 ###############################################################################
 
 
